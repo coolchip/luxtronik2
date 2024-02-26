c8cce809facd85e6add6b1a771fb864e1f7fbc7a
Sebastian Balling <coolchip@gmx.de>
2024-02-26 20:50:18 +0100

    write captured data to disc when using example.js

--- example.js
+++ example.js
@@ -8,6 +8,8 @@ pump.read(function (err, data) {
     }
     console.log(data);
     console.log(data.values.errors);
+
+    require('fs').writeFileSync('data.json', JSON.stringify(data, null, 4));
 });
 /*
 pump.write('heating_target_temperature', 0, function (err, data) {
